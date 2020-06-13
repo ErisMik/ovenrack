9154ea601da8a67e8820e5063e4b251ec5699828
Eric Mikulin <ericm99@gmail.com>
2020-06-12 21:27:30 -0700

    Playing with the DNS stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,128 @@
-use etherparse::InternetSlice;
-use etherparse::SlicedPacket;
-use etherparse::TransportSlice;
+use byteorder::{ByteOrder, NetworkEndian};
+use etherparse::{SlicedPacket, TransportSlice};
 use pcap::Device;
 
+// struct DnsQuestion {
+//     qname_raw: Vec<u8>,
+//     qname: Vec<Vec<char>>,
+//     qtype: u16,
+//     qclass: u16,
+// }
+
+struct DnsHeader {
+    id: u16,
+    flags: u16,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
 fn parse_dns(payload: &[u8]) {
-    print!("DNS Query: ");
-    if let Ok(payload_string) = std::str::from_utf8(payload) {
-        println!("{}", payload_string);
+    let dns_header = DnsHeader {
+        id: NetworkEndian::read_u16(&payload[0..2]),
+        flags: NetworkEndian::read_u16(&payload[2..4]),
+        qdcount: NetworkEndian::read_u16(&payload[4..6]),
+        ancount: NetworkEndian::read_u16(&payload[6..8]),
+        nscount: NetworkEndian::read_u16(&payload[8..10]),
+        arcount: NetworkEndian::read_u16(&payload[10..12]),
+    };
+
+    let mut payload_offset = 12;
+
+    let mut dns_questions = 0;
+    while dns_questions < dns_header.qdcount {
+        print!("Question for: {} ", dns_header.id);
+
+        loop {
+            let qname_field_len = payload[payload_offset];
+            payload_offset += 1;
+
+            if qname_field_len == 0 {
+                break;
+            }
+
+            let payload_offset_end = payload_offset + qname_field_len as usize;
+            let qname_field =
+                std::str::from_utf8(&payload[payload_offset..payload_offset_end]).unwrap();
+            payload_offset = payload_offset_end;
+            print!("{}.", qname_field);
+        }
+
+        let mut payload_offset_end = payload_offset + 2;
+        print!(
+            " type: {}",
+            NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end])
+        );
+        payload_offset = payload_offset_end;
+
+        payload_offset_end += 2;
+        print!(
+            " class: {}",
+            NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end])
+        );
+        payload_offset = payload_offset_end;
+
+        dns_questions += 1;
+        println!(";");
+    }
+
+    let mut dns_answers = 0;
+    while dns_answers < dns_header.ancount {
+        print!("Answer for: {} ", dns_header.id);
+
+        loop {
+            let qname_field_len = payload[payload_offset];
+            payload_offset += 1;
+
+            if qname_field_len == 0 {
+                break;
+            } else if qname_field_len == 192 {
+                payload_offset += 1;
+                break;
+            }
+
+            let payload_offset_end = payload_offset + qname_field_len as usize;
+            let qname_field =
+                std::str::from_utf8(&payload[payload_offset..payload_offset_end]).unwrap();
+            payload_offset = payload_offset_end;
+            print!("{}.", qname_field);
+        }
+
+        let mut payload_offset_end = payload_offset + 2;
+        print!(
+            " type: {}",
+            NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end])
+        );
+        payload_offset = payload_offset_end;
+
+        payload_offset_end += 2;
+        print!(
+            " class: {}",
+            NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end])
+        );
+        payload_offset = payload_offset_end;
+
+        payload_offset_end += 4;
+        print!(
+            " ttl: {}",
+            NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end])
+        );
+        payload_offset = payload_offset_end;
+
+        payload_offset_end += 2;
+        let rdlen = NetworkEndian::read_u16(&payload[payload_offset..payload_offset_end]);
+        print!(" len: {}", rdlen);
+        payload_offset = payload_offset_end;
+
+        payload_offset_end += rdlen as usize;
+        print!(
+            " data: {:?}",
+            &payload[payload_offset..payload_offset_end]
+        );
+
+        dns_answers += 1;
+        println!(";");
     }
 }
 
@@ -22,45 +138,23 @@ fn main() {
         match SlicedPacket::from_ethernet(&packet.data) {
             Err(value) => println!("Err {:?}", value),
             Ok(slicedpacket) => {
-                // match slicedpacket.ip {
-                //     None => println!("Missing IP slice"),
-                //     Some(ip) => match ip {
-                //         InternetSlice::Ipv4(ipheader) => println!(
-                //             "{:?} --> {:?}",
-                //             ipheader.source_addr(),
-                //             ipheader.destination_addr()
-                //         ),
-                //         InternetSlice::Ipv6(ipheader, _ipheaderext) => println!(
-                //             "{:?} --> {:?}",
-                //             ipheader.source_addr(),
-                //             ipheader.destination_addr()
-                //         ),
-                //     },
-                // }
-                match slicedpacket.transport {
-                    None => println!("Missing transport data"),
-                    Some(transport) => match transport {
+                if let Some(transport) = slicedpacket.transport {
+                    match transport {
                         TransportSlice::Udp(transportheader) => {
-                            // println!(
-                            //     "{} --> {}",
-                            //     transportheader.source_port(),
-                            //     transportheader.destination_port()
-                            // );
-                            if transportheader.destination_port() == 53 {
+                            if transportheader.destination_port() == 53
+                                || transportheader.source_port() == 53
+                            {
                                 parse_dns(slicedpacket.payload);
                             }
                         }
                         TransportSlice::Tcp(transportheader) => {
-                            // println!(
-                            //     "{} --> {}",
-                            //     transportheader.source_port(),
-                            //     transportheader.destination_port()
-                            // );
-                            if transportheader.destination_port() == 53 {
+                            if transportheader.destination_port() == 53
+                                || transportheader.source_port() == 53
+                            {
                                 parse_dns(slicedpacket.payload);
                             }
                         }
-                    },
+                    };
                 }
             }
         }
