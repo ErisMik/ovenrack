d91cff0c161feaf0a3a2867eb1336ccfb9fd077a
Eric Mikulin <ericm99@gmail.com>
2023-07-11 17:48:25 -0700

    Initial improved error handling

--- src/source.rs
+++ src/source.rs
@@ -1,6 +1,8 @@
 use std::net::UdpSocket;
 
 use log::*;
+use retry::delay::Fixed;
+use retry::*;
 
 use crate::cache::DnsCache;
 use crate::dns;
@@ -20,16 +22,27 @@ impl SourceServer {
 
     pub fn start(&mut self) {
         info!("Binding to: {}", self.addr);
-        let socket = UdpSocket::bind(self.addr.clone()).expect("Failed to bind UDP socket");
+        let socket = UdpSocket::bind(self.addr.clone())
+            .unwrap_or_else(|error| panic!("Failed to bind UDP socket `{}`: {error}", self.addr));
 
         loop {
             let mut buf = [0; 512];
-            let (_number_of_bytes, src_addr) = socket.recv_from(&mut buf).unwrap();
+            let (_number_of_bytes, src_addr) = match socket.recv_from(&mut buf) {
+                Ok(data) => data,
+                Err(error) => {
+                    error!("Failed to receive data from socket: {error}");
+                    continue;
+                }
+            };
 
             let dns_request = dns::DnsPacket::from_slice(&buf);
             if dns_request.header.isrequest() {
                 let dns_response = self.cache.query(dns_request);
-                socket.send_to(&dns_response.bytes(), src_addr).unwrap();
+                if let Err(error) = retry(Fixed::from_millis(25).take(3), || {
+                    socket.send_to(&dns_response.bytes(), src_addr)
+                }) {
+                    error!("Failed to send data from socket (tried 3 times): {error}");
+                }
             }
         }
     }
